af4aabbc567c10fc99660245e5bfc5d52d2eae7a
ppazosp <pablopazosp3@gmail.com>
2024-09-19 15:12:27 +0200

    finished bottom nav bar logic

--- app/src/main/java/ppazosp/changapp/MainActivity.kt
+++ app/src/main/java/ppazosp/changapp/MainActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import ppazosp.changapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(SearchFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId)
+            {
+                R.id.search -> replaceFragment(SearchFragment())
+                R.id.chats -> replaceFragment(ChatsFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                else -> {
+
+                }
+            }
+            true
+
         }
+    }
+
+    private fun replaceFragment( fragment : Fragment)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
